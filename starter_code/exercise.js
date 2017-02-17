6e751e25fed6cf11aa473f61accb459b8225a96e
Anuj Bheda <anujb89@gmail.com>
2017-02-17 18:55:36 -0500

    initial commit

---
+++ starter_code/exercise.js
@@ -0,0 +1,43 @@
+var exercise = {};
+
+
+exercise.roman = function(time){
+
+    //-------------------------------------
+    //  YOUR CODE
+    //    Return an object with roman time. 
+    //    Time is reported using 6 columns.
+    //
+    //  For example, for 05:13:47 PM
+    //    { 
+    //       hour_col1 : 1, 
+    //       hour_col2 : 7,
+    //       min_col1  : 1, 
+    //       min_col2  : 3, 
+    //       sec_col1  : 4, 
+    //       sec_col2  : 7
+    //     };
+    // 
+    //-------------------------------------
+
+};
+
+exercise.binary = function(time, col){
+
+    //----------------------------------------------------------
+    //  YOUR CODE
+    //    Return an object with
+    //    the binary clock values 
+    //    for the given column
+    //
+    //  For example, for time 05:13:47 PM, and column hour_col2
+    //    var binary = { 
+    //        position8 : 'off', 
+    //        position4 : 'on', 
+    //        position2 : 'on', 
+    //        position1 : 'on', 
+    //    }; 
+    // 
+    //----------------------------------------------------------
+
+};
