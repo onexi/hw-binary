6eed5a8287a71df5c63c3d885116b9ec179c0575
Mikayla <mikayla@mit.edu>
2017-02-23 11:47:22 -0500

    Pset 2

--- mikaylamurphy/exercise.js
+++ mikaylamurphy/exercise.js
@@ -21,19 +21,15 @@ exercise.roman = function(time){
     str_time = String(time);
     str_list = str_time.split(" ");
     time = str_list[4];
-    hour_col1 = time[0];
-    hour_col2 = time[1];
-    min_col1 = time[3];
-    min_col2 = time[4];
-    sec_col1 = time[6];
-    sec_col2 = time[7];
-    document.getElementById('hr1r').innerText = String(hour_col1);
-    document.getElementById('hr2r').innerText = String(hour_col2);
-    document.getElementById('min1r').innerText = String(min_col1);
-    document.getElementById('min2r').innerText = String(min_col2);
-    document.getElementById('sec1r').innerText = String(sec_col1);
-    document.getElementById('sec2r').innerText = String(sec_col2);
 
+    return {  
+        hour_col1 : parseInt(time[0]),
+        hour_col2 : parseInt(time[1]),
+        min_col1 : parseInt(time[3]),
+        min_col2 : parseInt(time[4]),
+        sec_col1 : parseInt(time[6]),
+        sec_col2 : parseInt(time[7]) 
+        };
 
 };
 
@@ -65,110 +61,99 @@ exercise.binary = function(time, col){
     sec_1 = time[6];
     sec_2 = time[7];
 
-
-    hr12 = document.getElementById('hr12');
-    hr11 = document.getElementById( 'hr11' );
-    hr28 = document.getElementById('hr28');
-    hr24 = document.getElementById( 'hr24' );
-    hr22 = document.getElementById('hr22');
-    hr21 = document.getElementById( 'hr21' );
-    min14 = document.getElementById('min14');
-    min12 = document.getElementById( 'min12' );
-    min11 = document.getElementById('min11');
-    min28 = document.getElementById( 'min28' );
-    min24 = document.getElementById('min24');
-    min22 = document.getElementById( 'min22' );
-    min21 = document.getElementById('min21');
-    sec14 = document.getElementById('sec14');
-    sec12 = document.getElementById('sec12' );
-    sec11 = document.getElementById('sec11');
-    sec28 = document.getElementById( 'sec28' );
-    sec24 = document.getElementById('sec24');
-    sec22 = document.getElementById( 'sec22' );
-    sec21 = document.getElementById('sec21');
-
-
-
-    if (col == 'hour_col_1'){
+    if (col == 'hour_col1'){
+        var binary = {position8: 'off', position4:'off', position2: 'off', position1: 'off'};
         if (hour_1 == 2){
-            hr12.style.backgroundColor = 'red';
+            binary.position2 = "on";
         } else if (hour_1 == 1) {
-            hr11.style.backgroundColor = 'red';
+            binary.position1 = "on";
+
         }
-    } else if (col == 'hour_col_2'){
+
+    } else if (col == 'hour_col2'){
+        var binary = {position8: 'off', position4: 'off', position2: 'off', position1: 'off'};
+
         if (hour_2 >= 8){
-            hr28.style.backgroundColor = 'red';
             hour_2 -= 8;
+            binary.position8 = "on";
         }
         if (hour_2 >= 4){
-            hr24.style.backgroundColor = 'red';
             hour_2 -= 4;
+            binary.position4 = "on";
         }
         if (hour_2 >= 2){
-            hr22.style.backgroundColor = 'red';
             hour_2 -= 2;
+            binary.position2 = "on";
         }
         if (hour_2 >= 1){
-            hr21.style.backgroundColor = 'red';
+            binary.position1 = "on";
         }
-    } else if (col == 'min_col_1'){
+    } else if (col == 'min_col1'){
+        var binary = {position8: 'off', position4: 'off', position2: 'off', position1: 'off'};
+
         if (min_1>= 4){
-            min14.style.backgroundColor = 'red';
             min_1 -= 4;
+            binary.position4 = "on";
         }
         if (min_1 >= 2){
-            min12.style.backgroundColor = 'red';
             min_1 -= 2;
+            binary.position2 = "on";
         }
         if (min_1 >= 1){
-            min11.style.backgroundColor = 'red';
+            binary.position1 = "on";
         }
 
-    } else if (col == 'min_col_2'){
+    } else if (col == 'min_col2'){
+        var binary = {position8: 'off', position4: 'off', position2: 'off', position1: 'off'};
+
         if (min_2 >= 8){
-            min28.style.backgroundColor = 'red';
             min_2 -= 8;
+            binary.position8 = "on";
         }
         if (min_2 >= 4){
-            min24.style.backgroundColor = 'red';
             min_2 -= 4;
+            binary.position4 = "on";
         }
         if (min_2 >= 2){
-            min22.style.backgroundColor = 'red';
             min_2 -= 2;
+            binary.position2 = "on";
         }
         if (min_2 >= 1){
-            min21.style.backgroundColor = 'red';
+            binary.position1 = "on";
         }
-    } else if (col == 'sec_col_1'){
+    } else if (col == 'sec_col1'){
+        var binary = {position8: 'off', position4: 'off', position2: 'off', position1: 'off'};
+
         if (sec_1>= 4){
-            sec14.style.backgroundColor = 'red';
             sec_1 -= 4;
+            binary.position4 = "on";
         }
         if (sec_1 >= 2){
-            sec12.style.backgroundColor = 'red';
             sec_1 -= 2;
+            binary.position2 = "on";
         }
         if (sec_1 >= 1){
-            sec11.style.backgroundColor = 'red';
+            binary.position1 = "on";
         }
 
-    } else if (col == 'sec_col_2'){
+    } else if (col == 'sec_col2'){
+        var binary = {position8: 'off', position4: 'off', position2: 'off', position1: 'off'};
+
         if (sec_2 >= 8){
-            sec28.style.backgroundColor = 'red';
             sec_2 -= 8;
+            binary.position8 = "on";
         }
         if (sec_2 >= 4){
-            sec24.style.backgroundColor = 'red';
             sec_2 -= 4;
+            binary.position4 = "on";
         }
         if (sec_2 >= 2){
-            sec22.style.backgroundColor = 'red';
             sec_2 -= 2;
+            binary.position2 = "on";
         }
         if (sec_2 >= 1){
-            sec21.style.backgroundColor = 'red';
+            binary.position1 = "on";
         }
     }
-
+    return binary;
 };
