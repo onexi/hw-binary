58e026678151652daa31b265e0eeb6b21e25020c
noclew <noclews@gmail.com>
2017-02-23 00:26:06 -0500

    v0.99 almost final

--- noclew/exercise.js
+++ noclew/exercise.js
@@ -1,5 +1,23 @@
 var exercise = {};
 
+exercise.drawBaseGrid = function () {
+
+    var idx = [8,4,2,1]
+    for (var index = 1; index < 7; index++) {
+        var posLeft = 50 + (index - 1) * 110;
+
+        for (var vIndex = 1; vIndex < 5; vIndex++) {
+            var div = document.createElement('div');
+            div.className = 'digitOff';      
+            div.style.left = posLeft + 'px';
+            div.style.posTop = 50 + (vIndex-1) * 110;
+            div.id = index + '-' + idx[ vIndex-1];
+            document.body.appendChild(div);
+        }
+    }
+}
+
+
 
 exercise.roman = function (time) {
 
@@ -56,23 +74,22 @@ exercise.binary = function (time, col) {
     else if (col === 'sec_col1') var myDigit = Number(Math.floor(time.getSeconds() / 10));
     else if (col === 'sec_col2') var myDigit = Number(time.getSeconds() % 10);
 
-    
+
     //myDigit = time;
-    if ((col !== 'hour_col1') && (col !== 'min_col1') && (col !== 'sec_col1')) {
-        if (myDigit >= 8) {
-            binary.position8 = 'on';
-            myDigit -= 8;
-        }
-        else { binary.position8 = 'off'; }
+    // if ((col !== 'hour_col1') && (col !== 'min_col1') && (col !== 'sec_col1')) {
+    if (myDigit >= 8) {
+        binary.position8 = 'on';
+        myDigit -= 8;
     }
 
-    if ((col !== 'hour_col1')) {
-        if (myDigit >= 4) {
-            binary.position4 = 'on';
-            myDigit -= 4;
-        }
-        else binary.position4 = 'off';
+    else { binary.position8 = 'off'; }
+
+    if (myDigit >= 4) {
+        binary.position4 = 'on';
+        myDigit -= 4;
     }
+    else binary.position4 = 'off';
+
 
     if (myDigit >= 2) {
         binary.position2 = 'on';
@@ -86,7 +103,7 @@ exercise.binary = function (time, col) {
     }
     else binary.position1 = 'off';
 
-    
+
     return binary;
 
 };
