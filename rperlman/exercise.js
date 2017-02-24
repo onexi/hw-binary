e88bd52d58527ae8928c87b6f9ab6da330ed5029
rperlman <rachperlman@gmail.com>
2017-02-24 00:07:58 -0500

    late thurs night update

--- rperlman/exercise.js
+++ rperlman/exercise.js
@@ -1,9 +1,10 @@
-//Rachel Perlman
+// 1.001 Homework 2
+//Author: Rachel Perlman
 
 var exercise = {};
 
-
 exercise.roman = function(time){
+// return an object with roman time, time is eported using 6 columns
     var roman = {
         hour_col1: 0,
         hour_col2: 0,
@@ -12,49 +13,29 @@ exercise.roman = function(time){
         sec_col1: 0,
         sec_col2: 0
     };
+    // parce hours into two digits
     var hours = time.getHours();
     roman.hour_col1 = Math.floor(hours/10);
     roman.hour_col2 = hours % 10;
 
-
+    // parce minutes into two digits
     var mins = time.getMinutes();
     roman.min_col1 = Math.floor(mins/10);
     roman.min_col2 = mins % 10;
     
-
+    // parce seconds into two digits
     var secs = time.getSeconds();
     roman.sec_col1 = Math.floor(secs/10);
     roman.sec_col2 = secs % 10;
 
-    return roman;
-    
-    //-------------------------------------
-    //  YOUR CODE
-    //    Return an object with roman time. 
-    //    Time is reported using 6 columns.
-    //
-    //  For example, for 05:13:47 PM
-    //    { 
-    //       hour_col1 : 1, 
-    //       hour_col2 : 7,
-    //       min_col1  : 1, 
-    //       min_col2  : 3, 
-    //       sec_col1  : 4, 
-    //       sec_col2  : 7
-    //     };
-    // 
-    //-------------------------------------
+    return roman;   
+
 };
 
 exercise.binary = function(time, col){
-    // var digit;
-    // if (col == 'hour_col1'){digit = exercise.roman.hour_col1;}
-    // if (col == 'hour_col2'){digit = exercise.roman.hour_col2;}
-    // if (col == 'min_col1'){digit = exercise.roman.min_col1;}
-    // if (col == 'min_col2'){digit = exercise.roman.min_col2;}
-    // if (col == 'sec_col1'){digit = exercise.roman.sec_col1;}
-    // if (col == 'sec_col2'){digit = exercise.roman.sec_col2;} 
+// Return an object with the binary clock values for the given column
 
+// start with all block positions off
     var binary = {
         position8: 'off',
         position4: 'off',
@@ -81,21 +62,5 @@ exercise.binary = function(time, col){
     };
 
     return binary;
-
-    //----------------------------------------------------------
-    //  YOUR CODE
-    //    Return an object with
-    //    the binary clock values 
-    //    for the given column
-    //
-    //  For example, for time 05:13:47 PM, and column hour_col2
-    //    var binary = { 
-    //        position8 : 'off', 
-    //        position4 : 'on', 
-    //        position2 : 'on', 
-    //        position1 : 'on', 
-    //    }; 
-    // 
-    //----------------------------------------------------------
 
 };
