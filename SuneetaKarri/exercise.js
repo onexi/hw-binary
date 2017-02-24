03a990d05b168d9984929681504661c51a88f4b9
Suneeta Karri <suneetak@mit.edu>
2017-02-23 22:47:35 -0500

    Binary time under progress
    Roman time working, working on the binary part

--- SuneetaKarri/exercise.js
+++ SuneetaKarri/exercise.js
@@ -20,9 +20,37 @@ exercise.roman = function(time){
     // 
     //-------------------------------------
 
-};
+var hours = ("00" + time.getHours()).slice(-2);
+    minutes = ("00" + time.getMinutes()).slice(-2);
+    seconds = ("00" + time.getSeconds()).slice(-2);
+
+  return{
+
+   hour_col51 : hours.slice(0,1),
+   hour_col52 : hours.slice(1,2),
+   min_col53 : minutes.slice(0,1),
+   min_col54 : minutes.slice(1,2),
+   sec_col55 : seconds.slice(0,1),
+   sec_col56 : seconds.slice(1,2)
+   };
+    };
 
 exercise.binary = function(time, col){
+    var hours = ("00" + time.getHours()).slice(-2);
+    minutes = ("00" + time.getMinutes()).slice(-2);
+    seconds = ("00" + time.getSeconds()).slice(-2);
+
+  return{
+
+   hour_col1 : hours.slice(0,1),
+   hour_col2 : hours.slice(1,2),
+   min_col1 : minutes.slice(0,1),
+   min_col2 : minutes.slice(1,2),
+   sec_col1 : seconds.slice(0,1),
+   sec_col2 : seconds.slice(1,2)
+   };
+
+
 
     //----------------------------------------------------------
     //  YOUR CODE
