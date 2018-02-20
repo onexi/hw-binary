089d95c3a1263d8913c25e047bfdcf49b401aa66
prateekkuk <prateek.kukreja@gmail.com>
2018-02-20 12:28:42 -0500

    basic functions

--- prateekkuk/exercise.js
+++ prateekkuk/exercise.js
@@ -39,5 +39,64 @@ exercise.binary = function(time, col){
     //    }; 
     // 
     //----------------------------------------------------------
-
+    var binary = {}
+    var timeString = "";
+    switch(col){
+        case "col1":
+            timeString = time.charAt(0);
+        break;
+        case "col2":
+            timeString = time.charAt(1);
+        break;
+        case "col3":
+            timeString = time.charAt(3); //skipping the :
+        break;
+        case "col4":
+            timeString = time.charAt(4);
+        break;
+        case "col5":
+            timeString = time.charAt(6);
+        break;
+        case "col6":
+            timeString = time.charAt(7);
+        default:
+            console.log("default case in col picker");
+    }
+    switch(timeString){
+        case "0": 
+            binary.position8 =  'off'; 
+            binary.position4 =  'off';
+            binary.position2 =  'off';
+            binary.position1 =  'off';
+        break;
+        case "1":
+            binary.position8 =  'off'; 
+            binary.position4 =  'off';
+            binary.position2 =  'off';
+            binary.position1 =  'on';
+            document.getElementById("hour_"+col+"_pos1").style.background = "red";
+        break;
+        case "2":
+            binary.position8 =  'off'; 
+            binary.position4 =  'off';
+            binary.position2 =  'on';
+            binary.position1 =  'off';
+            document.getElementById("hour_"+col+"_pos2").style.background = "red";
+        break;
+        case "3":
+        break;
+        case "4":
+        break;
+        case "5":
+        break;
+        case "6":
+        break;
+        case "7":
+        break;
+        case "8":
+        break;
+        case "9":
+        break;
+    }
+    return binary;
 };
