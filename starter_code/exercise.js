29e94704310bdf753e0e9fafdde79801a06189b0
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2018-02-20 15:38:17 -0500

    Removed Trailing Whitespaces

--- starter_code/exercise.js
+++ starter_code/exercise.js
@@ -5,19 +5,19 @@ exercise.roman = function(time){
 
     //-------------------------------------
     //  YOUR CODE
-    //    Return an object with roman time. 
+    //    Return an object with roman time.
     //    Time is reported using 6 columns.
     //
     //  For example, for 05:13:47 PM
-    //    { 
-    //       hour_col1 : 1, 
+    //    {
+    //       hour_col1 : 1,
     //       hour_col2 : 7,
-    //       min_col1  : 1, 
-    //       min_col2  : 3, 
-    //       sec_col1  : 4, 
+    //       min_col1  : 1,
+    //       min_col2  : 3,
+    //       sec_col1  : 4,
     //       sec_col2  : 7
     //     };
-    // 
+    //
     //-------------------------------------
 
 };
@@ -27,17 +27,17 @@ exercise.binary = function(time, col){
     //----------------------------------------------------------
     //  YOUR CODE
     //    Return an object with
-    //    the binary clock values 
+    //    the binary clock values
     //    for the given column
     //
     //  For example, for time 05:13:47 PM, and column hour_col2
-    //    var binary = { 
-    //        position8 : 'off', 
-    //        position4 : 'on', 
-    //        position2 : 'on', 
-    //        position1 : 'on', 
-    //    }; 
-    // 
+    //    var binary = {
+    //        position8 : 'off',
+    //        position4 : 'on',
+    //        position2 : 'on',
+    //        position1 : 'on',
+    //    };
+    //
     //----------------------------------------------------------
 
 };
