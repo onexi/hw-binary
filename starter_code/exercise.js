4622c7bba99cffe2d34f90776d4e2610a60e83b0
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2018-02-20 16:45:18 -0500

    Implemented Binary Counter & Polyfill for padStart
    padStart is available in the browser but not in Node.JS

--- starter_code/exercise.js
+++ starter_code/exercise.js
@@ -1,3 +1,23 @@
+// Polyfill for Node.JS which has not implemented padStart()
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength,padString) {
+        targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return padString.slice(0,targetLength) + String(this);
+        }
+    };
+}
+
 var exercise = {};
 
 
@@ -17,21 +37,12 @@ exercise.roman = function(time){
 };
 
 exercise.binary = function(time, col){
-
-    //----------------------------------------------------------
-    //  YOUR CODE
-    //    Return an object with
-    //    the binary clock values
-    //    for the given column
-    //
-    //  For example, for time 05:13:47 PM, and column hour_col2
-    //    var binary = {
-    //        position8 : 'off',
-    //        position4 : 'on',
-    //        position2 : 'on',
-    //        position1 : 'on',
-    //    };
-    //
-    //----------------------------------------------------------
-
+    var romanFormatted = exercise.roman(time);
+    var binaryRepr = parseInt(romanFormatted[col]).toString(2).padStart(4,0);
+    var binary = {};
+    var binaryKeys = ['position8', 'position4', 'position2', 'position1'];
+    for (var i = 0; i <= binaryKeys.length - 1; i++) {
+        binary[binaryKeys[i]] = (binaryRepr[i] == "1")?'on':'off';
+    }
+    return binary;
 };
