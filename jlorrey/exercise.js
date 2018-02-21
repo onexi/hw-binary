6065a40ac7926837b4b42eb96669c8b7f61f93ad
jlorrey <jlorrey@mit.edu>
2018-02-20 21:05:18 -0500

    Updated exercise file(s)

--- jlorrey/exercise.js
+++ jlorrey/exercise.js
@@ -25,12 +25,12 @@ exercise.roman = function(time){
     seconds = String(time.getSeconds());
 
     var romanCols = {
-        hour_col1 : Number(hours[0]), //index string, then make that value a number
-        hour_col2 : Number(hours[1]),
-        min_col1  : Number(minutes[0]), 
-        min_col2  : Number(minutes[1]),
-        sec_col1  : Number(seconds[0]), 
-        sec_col2  : Number(seconds[1])
+        "hour_col1" : Number(hours[0]), //index string, then make that value a number
+        "hour_col2" : Number(hours[1]),
+        "min_col1"  : Number(minutes[0]), 
+        "min_col2"  : Number(minutes[1]),
+        "sec_col1"  : Number(seconds[0]), 
+        "sec_col2"  : Number(seconds[1])
     };
     return romanCols;
 };
