1ee932ab2e6d2e723ea45b8e40a8d71c7d474f22
jlorrey <jlorrey@mit.edu>
2018-02-20 22:01:25 -0500

    Updated exercise file(s)

--- jlorrey/exercise.js
+++ jlorrey/exercise.js
@@ -18,19 +18,19 @@ exercise.roman = function(time){
     //       sec_col1  : 4, 
     //       sec_col2  : 7
     //     };
-    //  Looks like a javascript object b/c JSON objects require quots around keys
+    //  
     //-------------------------------------
     hours   = String(time.getHours()); //make string for indexing
     minutes = String(time.getMinutes());
     seconds = String(time.getSeconds());
 
     var romanCols = {
-        "hour_col1" : Number(hours[0]), //index string, then make that value a number
-        "hour_col2" : Number(hours[1]),
-        "min_col1"  : Number(minutes[0]), 
-        "min_col2"  : Number(minutes[1]),
-        "sec_col1"  : Number(seconds[0]), 
-        "sec_col2"  : Number(seconds[1])
+        hour_col1 : Number(hours[0]), //index string, then make that value a number
+        hour_col2 : Number(hours[1]),
+        min_col1  : Number(minutes[0]), 
+        min_col2  : Number(minutes[1]),
+        sec_col1  : Number(seconds[0]), 
+        sec_col2  : Number(seconds[1])
     };
     return romanCols;
 };
@@ -51,16 +51,17 @@ exercise.binary = function(time, col){
     //        position1 : 'on', 
     //    };    
     //----------------------------------------------------------
-    // romanCols = exercise.roman(time); //unclear if this is given in col or not
-    // romanVal = romanCols.col; //the roman digit associated with column
-    // binaryVal = romanVal.toString(2); //convert to binary string (bonus: can index strings)
+    romanCols = exercise.roman(time); 
+    romanVal = romanCols.col; //the roman digit associated with column
+    binaryVal = romanVal.toString(2); //convert to binary string (bonus: can index strings)
     
-    // stateArray = ['off', 'on']; //will use to map binary (0, 1) to ('off', 'on') 
+    stateArray = ['off', 'on']; //will use to map binary (0, 1) to ('off', 'on') 
     
-    // var binary = {
-    //     "position8" : stateArray[binaryVal[3]],
-    //     "position4" : stateArray[binaryVal[2]],
-    //     "position2" : stateArray[binaryVal[1]],
-    //     "position1" : stateArray[binaryVal[0]]
-    // };
+    var binary = {                            //e.g. if romanVal = 7 --> binaryVal = '0111'
+        position8 : stateArray[Boolean(binaryVal[0])], // binaryVal[0]=0 > bool=0 > stateArray[0]='off'
+        position4 : stateArray[Boolean(binaryVal[1])], // binaryVal[1]=1 > bool=1 > stateArray[1]='on'
+        position2 : stateArray[Boolean(binaryVal[2])], // binaryVal[2]=1 > bool=1 > stateArray[1]='on'
+        position1 : stateArray[Boolean(binaryVal[3])]  // binaryVal[3]=1 > bool=1 > stateArray[1]='on'
+    };
+    return binary;
 };
