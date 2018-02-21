a25be9ca13b2117f1a98c28994762a6cfb67e70f
jlorrey <jlorrey@mit.edu>
2018-02-20 20:28:34 -0500

    Updated exercise file(s)

--- jlorrey/exercise.js
+++ jlorrey/exercise.js
@@ -24,25 +24,17 @@ exercise.roman = function(time){
     minutes = String(time.getMinutes());
     seconds = String(time.getSeconds());
 
-    var romanTime = new Object();
-    {
-        romanTime.hour_col1 = Number(hours[0]);
-        romanTime.hour_col2 = Number(hours[1]);
-        romanTime.min_col1  = Number(minutes[0]);
-        romanTime.min_col1  = Number(minutes[1]);
-        romanTime.sec_col1  = Number(seconds[0]);
-        romanTime.sec_col2  = Number(seconds[1]);
-    }
-    return romanTime;
-    // var romanObj = {
-    //     hour_col1 : Number(hours[0]), //index string, then make that value a number
-    //     hour_col2 : Number(hours[1]),
-    //     min_col1  : Number(minutes[0]), 
-    //     min_col2  : Number(minutes[1]),
-    //     sec_col1  : Number(seconds[0]), 
-    //     sec_col2  : Number(seconds[1])
-    // };
-    // return romanObj;
+
+    var romanObj = {
+        "hour_col1" : Number(hours[0]), //index string, then make that value a number
+        "hour_col2" : Number(hours[1]),
+        "min_col1"  : Number(minutes[0]), 
+        "min_col2"  : Number(minutes[1]),
+        "sec_col1"  : Number(seconds[0]), 
+        "sec_col2"  : Number(seconds[1])
+    };
+    return romanObj;
+
 };
 
 exercise.binary = function(time, col){
