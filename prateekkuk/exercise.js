f63a88afc233e82c14ea36dd6e30f02bacb00842
prateekkuk <prateek.kukreja@gmail.com>
2018-02-21 17:05:07 -0500

    Updated exercise file(s)

--- prateekkuk/exercise.js
+++ prateekkuk/exercise.js
@@ -1,4 +1,5 @@
 var exercise = {};
+// var time = "";
 
 
 exercise.roman = function(time){
@@ -20,15 +21,13 @@ exercise.roman = function(time){
     // 
     //-------------------------------------
         var romanTime = {
-            "hour_col1":time[0],
-            "hour_col2":time[1],
-            "min_col1":time[3],
-            "min_col2":time[4],
-            "sec_col1":time[6],
-            "sec_col2":time[7]
+            hour_col1:time[0],
+            hour_col2:time[1],
+            min_col1:time[3],
+            min_col2:time[4],
+            sec_col1:time[6],
+            sec_col2:time[7]
         };
-      
-
         return romanTime;
 
 };
@@ -50,26 +49,30 @@ exercise.binary = function(time, col){
     //    }; 
     // 
     //----------------------------------------------------------
-    var binary = {
-        // "hour_col1":(getPositions(col)),
-        // "hour_col2":(getPositions(col)),
-        // "min_col1":(getPositions(col)),
-        // "min_col2":(getPositions(col)),
-        // "sec_col1":(getPositions(col)),
-        // "sec_col2":(getPositions(col)),
-        [col]:(getPositions(time,col)),
-    };
- 
+    //var test = {[col]:(getPositions(time,col)),};
+    // col.forEach(getPositions);
+    // console.log(binary)
+
+    var binary = {};
+    binary = getPositions(time,col);
     return binary;
 };
 
+// function getPositions(currentValue){
+//    var col = currentValue;
+//    var timeString = getLabel(time,col);
+//    var positions = {};
+//    positions = positionSetting(timeString,col);
+//    return positions;
+// }
 function getPositions(time,col){
-   var timeString = getLabel(time,col);
-   var positions = {};
-   positions = positionSetting(timeString,col);
-   return positions;
-}
-function getLabel(time,col){
+    var timeString = getTimeValue(time,col);
+    var positions = {};
+    positions = positionSetting(timeString,col);
+    return positions;
+ }
+ //this gets the value of what the time column should be based on where it is in the time string
+function getTimeValue(time,col){
     var timeString = "";
     switch(col){
         case "hour_col1":
@@ -100,99 +103,91 @@ function positionSetting(timeString,col){
     switch(timeString){
         case "0": 
             var binary = {
-                "position8":  'off', 
-                "position4": 'off',
-                "position2":  'off',
-                "position1":  'off',
-                [col]:timeString
+                position8:  'off', 
+                position4: 'off',
+                position2:  'off',
+                position1:  'off',
 
             };
         break;
         case "1":
             var binary = {
-                "position8":  'off', 
-                "position4": 'off',
-                "position2":  'off',
-                "position1":  'on',
-                [col]:timeString
+                position8:  'off', 
+                position4: 'off',
+                position2:  'off',
+                position1:  'on',
 
             };
         break;
         case "2":
             var binary = {
-                "position8":  'off', 
-                "position4": 'off',
-                "position2":  'on',
-                "position1":  'off',
-                [col]:timeString
+                position8:  'off', 
+                position4: 'off',
+                position2:  'on',
+                position1:  'off',
 
             };
         break;
         case "3":
             var binary = {
-                "position8":  'off', 
-                "position4": 'off',
-                "position2":  'on',
-                "position1":  'on',
-                [col]:timeString
+                position8:  'off', 
+                position4: 'off',
+                position2:  'on',
+                position1:  'on',
 
             };
         break;
         case "4":
             var binary = {
-                "position8":  'off', 
-                "position4": 'on',
-                "position2":  'off',
-                "position1":  'off',
-                [col]:timeString
+                position8:  'off', 
+                position4: 'on',
+                position2:  'off',
+                position1:  'off',
 
             };
         break;
         case "5":
             var binary = {
-                "position8":  'off', 
-                "position4": 'on',
-                "position2":  'off',
-                "position1":  'on',
-                [col]:timeString
+                position8:  'off', 
+                position4: 'on',
+                position2:  'off',
+                position1:  'on',
 
             };
         break;
         case "6":
             var binary = {
-                "position8":  'off', 
-                "position4": 'on',
-                "position2":  'on',
-                "position1":  'off',
-                [col]:timeString
+                position8: 'off', 
+                position4: 'on',
+                position2: 'on',
+                position1: 'off',
 
             };
         break;
         case "7":
             var binary = {
-                "position8":  'off', 
-                "position4": 'on',
-                "position2":  'on',
-                "position1":  'on',
-                [col]:timeString
+                position8:  'off', 
+                position4: 'on',
+                position2:  'on',
+                position1:  'on',
 
             };
         break;
         case "8":
             var binary = {
-                "position8":  'on', 
-                "position4": 'off',
-                "position2":  'off',
-                "position1":  'off',
+                position8:  'on', 
+                position4: 'off',
+                position2:  'off',
+                position1:  'off',
 
             };
         break;
         case "9":
             var binary = {
-                "position8":  'on', 
-                "position4": 'off',
-                "position2":  'off',
-                "position1":  'on',
+                position8:  'on', 
+                position4: 'off',
+                position2:  'off',
+                position1:  'on',
 
             };
         break;
