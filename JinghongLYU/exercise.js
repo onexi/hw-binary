fbc8b28ec0f0764ac0c6cc079ad792ccfcc47128
JinghongLYU <lyujinghong@gmail.com>
2018-02-22 16:21:51 -0500

    Updated exercise file(s)

--- JinghongLYU/exercise.js
+++ JinghongLYU/exercise.js
@@ -3,22 +3,9 @@ var exercise = {};
 
 exercise.roman = function(time){
 
-    //-------------------------------------
-    //  YOUR CODE
     //    Return an object with roman time. 
     //    Time is reported using 6 columns.
-    //
-    //  For example, for 05:13:47 PM
-    //    { 
-    //       hour_col1 : 1, 
-    //       hour_col2 : 7,
-    //       min_col1  : 1, 
-    //       min_col2  : 3, 
-    //       sec_col1  : 4, 
-    //       sec_col2  : 7
-    //     };
-    // 
-    //-------------------------------------
+
     var stamp = {
         hour_col1: Math.floor(time.getHours()/10),
         hour_col2: (time.getHours() % 10),
@@ -32,21 +19,10 @@ exercise.roman = function(time){
 
 exercise.binary = function(time, col){
 
-    //----------------------------------------------------------
-    //  YOUR CODE
     //    Return an object with
     //    the binary clock values 
     //    for the given column
-    //
-    //  For example, for time 05:13:47 PM, and column hour_col2
-    //    var binary = { 
-    //        position8 : 'off', 
-    //        position4 : 'on', 
-    //        position2 : 'on', 
-    //        position1 : 'on', 
-    //    }; 
-    // 
-    //----------------------------------------------------------
+
     var stamp  = exercise.roman(time);
     var number = stamp[col];
 
