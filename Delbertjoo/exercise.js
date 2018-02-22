9d512d82ce3b50be76f3ab2e86288a940c0c4600
Delbertjoo <dsjoo@mit.edu>
2018-02-22 11:18:53 -0500

    Updated exercise file(s)

--- Delbertjoo/exercise.js
+++ Delbertjoo/exercise.js
@@ -2,6 +2,48 @@ var exercise = {};
 
 
 exercise.roman = function(time){
+    var roman = {
+        hour_col1: 0,
+        hour_col2: 0,
+        min_col1: 0,
+        min_col2: 0,
+        sec_col1: 0,
+        sec_col2: 0,
+    };
+
+    hour = time.getHours();
+        if(hour<10){
+        exercise.roman.hour_col1 = 0;
+        exercise.roman.hour_col2 = hour.toString()[0];
+        }
+        else{
+        exercise.roman.hour_col1 = hour.toString()[0];
+        exercise.roman.hour_col2 = hour.toString()[1];
+        }
+
+    min = time.getMinutes();
+        if(min<10){
+        exercise.roman.min_col1 = 0;
+        exercise.roman.min_col2 = min.toString()[0];
+        }
+        else{
+        exercise.roman.min_col1 = min.toString()[0];
+        exercise.roman.min_col2 = min.toString()[1];
+        }
+
+    
+    sec = time.getSeconds();
+        if(sec<10){
+        exercise.roman.sec_col1 = 0;
+        exercise.roman.sec_col2 = sec.toString()[0];
+        }
+        else{
+        exercise.roman.sec_col1 = sec.toString()[0];
+        exercise.roman.sec_col2 = sec.toString()[1];
+        }
+
+
+    
 
     //-------------------------------------
     //  YOUR CODE
