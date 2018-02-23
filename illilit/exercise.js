62f96db309b008faaeeaccdc65f9e8c2aee04c5d
illilit <illilit@mit.edu>
2018-02-22 23:54:54 -0500

    Updated exercise file(s)

--- illilit/exercise.js
+++ illilit/exercise.js
@@ -3,23 +3,6 @@ var exercise = {};
 
 exercise.roman = function(time){
 
-    //-------------------------------------
-    //  YOUR CODE
-    //    Return an object with roman time. 
-    //    Time is reported using 6 columns.
-    //
-    //  For example, for 05:13:47 PM
-    //    { 
-    //       hour_col1 : 1, 
-    //       hour_col2 : 7,
-    //       min_col1  : 1, 
-    //       min_col2  : 3, 
-    //       sec_col1  : 4, 
-    //       sec_col2  : 7
-    //     };
-    // 
-    //-------------------------------------
-
     var hrs = time.getHours();
     var hrs_string;
     if(hrs<10){hrs_string = "0" + String(hrs);}
@@ -45,14 +28,9 @@ exercise.roman = function(time){
         sec_col2  : sec_string[1]
     };
 
-    
-
 };
 
 exercise.binary = function(time, col){
-
-//var time2= new Date();
-//time2.getSeconds();
 
     var hrs = time.getHours();
     var hrs_string;
@@ -69,25 +47,9 @@ exercise.binary = function(time, col){
     var sec_string;
     if(sec<10){sec_string = "0" + String(sec);}
     else{sec_string =  String(sec);}
-//----------------------------------------------------------
-
-    //----------------------------------------------------------
-    //  YOUR CODE
-    //    Return an object with
-    //    the binary clock values 
-    //    for the given column
-    //
-    //  For example, for time 05:13:47 PM, and column hour_col2
-    //    var binary = { 
-    //        position8 : 'off', 
-    //        position4 : 'on', 
-    //        position2 : 'on', 
-    //        position1 : 'on', 
-    //    }; 
-    // 
-    //----------------------------------------------------------
 
 //----------------------------------------------------------
+
     var binary0 = {
         position8: 'off',
         position4: 'off',
@@ -161,10 +123,7 @@ exercise.binary = function(time, col){
     var binary=[binary0,binary1,binary2,binary3,binary4,binary5,binary6,binary7,binary8,binary9];
 //----------------------------------------------------------
 
-//document.getElementById('hour_col1_pos1').style.backgroundColor = 'red';
-
-
-if (col === 1) {return binary[Number(hrs_string[0])];} // returning  
+if (col === 1) {return binary[Number(hrs_string[0])];}  
 if (col === 2) {return binary[Number(hrs_string[1])];}
 if (col === 3) {return binary[Number(min_string[0])];}
 if (col === 4) {return binary[Number(min_string[1])];}
