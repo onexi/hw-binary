a1a21a3f9c8a4a0bc648558c263b3aec9947405a
wuyueqz <wuyueqz@gmail.com>
2018-02-22 23:14:44 -0500

    Updated exercise file(s)

--- wuyueqz/exercise.js
+++ wuyueqz/exercise.js
@@ -19,7 +19,18 @@ exercise.roman = function(time){
     //     };
     // 
     //-------------------------------------
-
+    var hour = time.getHours();
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
+    var stamp = {
+    hour_col1 : Math.floor(hour/10),
+    hour_col2 : Math.floor(hour%10),
+    min_col1 : Math.floor(min/10),
+    min_col2 : Math.floor(min%10),
+    sec_col1 : Math.floor(sec/10),
+    sec_col2 : Math.floor(sec%10),
+};
+    return stamp;
 };
 
 exercise.binary = function(time, col){
@@ -39,5 +50,30 @@ exercise.binary = function(time, col){
     //    }; 
     // 
     //----------------------------------------------------------
+    var stamp = exercise.roman(time);
+    number = stamp[col];
+    var binary = {
+        position8 : 'off',
+        position4 : 'off',
+        position2 : 'off',
+        position1 : 'off', 
+    };
+    if (number >= 8){
+        binary.position8 = 'on';
+        number -= 8;
+    }
+    if (number >= 4){
+        binary.position4 = 'on';
+        number -= 4;
+    }
+    if (number >= 2){
+        binary.position2 = 'on';
+        number -= 2;
+    }
+    if (number >= 1){
+        binary.position1 = 'on';
+        number -= 1;
+    }
+   return binary;
 
 };
